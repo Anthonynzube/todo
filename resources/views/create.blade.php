e6edab3ff870e2e0333aa3192f8e4ba63418c940
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-13 16:17:17 +0100

    adjust create todo ui to make room for todo steps

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -17,6 +17,13 @@
             <div class="py-2">
                 <textarea name="description" class="py-2 px-4 border" placeholder="Description" /></textarea>
             </div>
+            <div class="flex justify-center py-2">
+                <h1 class="text-lg">Add Step(s) if required</h1>
+                <span class="fas fa-plus text-blue-600 py-2 px-2 cursor-pointer"/>
+            </div>
+            <div class="py-2">
+                <input type="text" name="step" class="py-2 px-2 border" placeholder="Describe Step"/>
+            < /div>
             <div class="py-2">
                 <input type="submit" value="Create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
             </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    @livewireStyles
     <title>Todo</title>
 </head>
 <body>
     @yield('content')
+    @livewireStyles
 </body>
 </html>
