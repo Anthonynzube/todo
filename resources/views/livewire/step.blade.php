15438f6a0d21a249a48c5c791f3f2d138b673b3a
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-14 10:57:58 +0100

    Added Ui functionality to remove and add steps

---
+++ resources/views/livewire/step.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="flex justify-center py-2">
+        <h1 class="text-lg">Add Step(s) if required</h1>
+        <span wire:click="increment" class="fas fa-plus text-blue-600 py-2 px-2 cursor-pointer"/>
+    </div>
+
+    @foreach($steps as $step)
+        <div class="py-2">
+            <input type="text" name="step[]" class="py-2 px-2 border" placeholder="{{'Describe Step '.$step}}"/>
+            <span class="fas fa-times text-red-600 px-2 cursor-pointer" wire:click="remove({{$loop->index}})"/>
+        </div>
+    @endforeach
+</div>
